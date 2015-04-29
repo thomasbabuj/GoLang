a8a1e6b779df6c883d88dede03b012d9b3754a90
Thomas Babu <tommy.developer@gmail.com>
2015-04-29 16:51:45 +0800

    Length of a string

--- src/hello/main.go
+++ src/hello/main.go
@@ -8,11 +8,5 @@ func main() {
   // Go String
   atoz := "the quick brown fox jumps over the lazy dog \n"
 
-  // In Go we can use "_" as variable name to specify that we are only using one value.
-  for _, r := range atoz {
-
-    // Printing the index and chars
-    // Since we use _ we are only getting the chars and we omit the index of the char
-    fmt.Printf(" %c \n", r)
-  }
+  fmt.Printf("Length of ATOZ String : %d \n", len(atoz))
 }
