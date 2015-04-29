f6deb1f5bfdbaa2768e46a59d7f392099e7d4271
Thomas Babu <tommy.developer@gmail.com>
2015-04-29 16:59:28 +0800

    string with double quotes

--- src/hello/main.go
+++ src/hello/main.go
@@ -9,7 +9,7 @@ func main() {
   /* declaring string with backquotes '`' means, anything in between start and end backquotes will be treated as a full string. So we dont need to worry about escaping the strings for single or double quotes etc ..
   */
 
-  atoz := `the quick brown fox jumps over the lazy dog \n`
+  atoz := `the "quick" brown fox jumps over the lazy dog \n`
 
   // This example \n wont be removed since we declare the string with '`'
   fmt.Printf("String : %s \n", atoz)
