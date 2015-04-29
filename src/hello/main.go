861ef0b698da2235437612dba86e33ff96210a66
Thomas Babu <tommy.developer@gmail.com>
2015-04-29 16:17:55 +0800

    Zero values

--- src/hello/main.go
+++ src/hello/main.go
@@ -6,7 +6,9 @@ import (
 
 func main() {
   // Boolean
-  isTrue := false
+  // In Go if you declare a variable without assign any values then by default it gets "Zero values"
+  var isTrue bool
 
+  // Since we didn't assign any value for isTrue variable now we get false as our output
   fmt.Printf("Value : %t\n", isTrue)
 }
