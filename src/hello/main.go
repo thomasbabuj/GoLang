056fbe3fe740ffc5de90d1cdec5a9b1dea524e07
Thomas Babu <tommy.developer@gmail.com>
2015-04-29 16:42:00 +0800

    split string

--- src/hello/main.go
+++ src/hello/main.go
@@ -7,6 +7,6 @@ import (
 func main() {
   // Go String
   atoz := "the quick brown fox jumps over the lazy dog \n"
-
-  fmt.Printf("String: %s", atoz)
+  // split strings
+  fmt.Printf("String: %s \n", atoz[15:19])
 }
