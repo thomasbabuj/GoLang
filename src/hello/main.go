3dd07fe2a49d93e68ce3256adca8a6f7f2785393
Thomas Babu <tommy.developer@gmail.com>
2015-04-29 16:49:56 +0800

    Using _ to omit one of the return value and just printing chars without index

--- src/hello/main.go
+++ src/hello/main.go
@@ -8,8 +8,11 @@ func main() {
   // Go String
   atoz := "the quick brown fox jumps over the lazy dog \n"
 
-  for i, r := range atoz {
+  // In Go we can use "_" as variable name to specify that we are only using one value.
+  for _, r := range atoz {
+
     // Printing the index and chars
-    fmt.Printf("%d %c \n", i, r)
+    // Since we use _ we are only getting the chars and we omit the index of the char
+    fmt.Printf(" %c \n", r)
   }
 }
