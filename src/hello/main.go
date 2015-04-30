8e9b8c4f7b7051e22255d784e226d500fb642d50
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 12:32:18 +0800

    single return value

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,19 +4,15 @@ import (
   "fmt"
 )
 
-// since both two parameters are using same type
-// we can use the following syntax
-func printer(msg , msg2 string ,repeat int) {
-  for repeat > 0 {
-    fmt.Printf("%s", msg)
-    fmt.Printf(" %s", msg2)
-    fmt.Printf("\n")
-    repeat -= 1
-  }
+// Return a value
+func printer(msg string) error {
+  _, err := fmt.Printf("%s \n", msg)
+
+  return err
 }
 
 func main() {
 
-  // Defining functions in Go
-  printer("Hello", "world", 5)
+  printer("Hello, World");
+
 }
