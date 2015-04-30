46bef512548858e82644e4605c0a9f814a5b555b
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 12:10:47 +0800

    diff types as paras

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,12 +4,19 @@ import (
   "fmt"
 )
 
-func printer(msg string) {
-  fmt.Printf("%s \n", msg)
+// since both two parameters are using same type
+// we can use the following syntax
+func printer(msg , msg2 string ,repeat int) {
+  for repeat > 0 {
+    fmt.Printf("%s", msg)
+    fmt.Printf(" %s", msg2)
+    fmt.Printf("\n")
+    repeat -= 1
+  }
 }
 
 func main() {
 
   // Defining functions in Go
-  printer("Hello, world")
+  printer("Hello", "world", 5)
 }
