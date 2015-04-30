c4534ca4af692dca7fadf1335920efc9b817c0ef
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 13:42:01 +0800

    Passing n number of params to a function using '...'

--- src/hello/main.go
+++ src/hello/main.go
@@ -5,24 +5,15 @@ import (
 )
 
 // Using Defer keyword
-// Declaring a variable called error "e"
-// implicitly return values with out specify variable name
-func printer(msg string) (returnedMessage string, e error) {
+// Passing unlimited nos of parameters to a function
 
-  // since we are getting value e here, this function automatically returns it.
-  _, e = fmt.Printf("%s\n", msg)
-  returnedMessage = "Yes I did it"
-  return
+func printer(msgs ...string) {
+  for _,msgs := range msgs {
+    fmt.Printf("%s \n", msgs)
+  }
 }
 
 func main() {
- value1,value2 := printer("Hello, World!")
-
-  if (value2 == nil ) {
-    fmt.Printf("I am in if \n")
-    fmt.Printf(" %s \n", value1 )
-  } else {
-    fmt.Printf("I am in else \n")
-    fmt.Printf(" %s %s", value1, value2 )
-  }
+  printer("Hello, world!", "How are u doing")
 }
+
