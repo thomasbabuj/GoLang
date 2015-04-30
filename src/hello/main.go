a8eef4abb694a9520f85e6a2ceac645aa25c2f64
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 13:38:23 +0800

    multiple return values

--- src/hello/main.go
+++ src/hello/main.go
@@ -7,13 +7,22 @@ import (
 // Using Defer keyword
 // Declaring a variable called error "e"
 // implicitly return values with out specify variable name
-func printer(msg string) (e error) {
+func printer(msg string) (returnedMessage string, e error) {
 
   // since we are getting value e here, this function automatically returns it.
   _, e = fmt.Printf("%s\n", msg)
+  returnedMessage = "Yes I did it"
   return
 }
 
 func main() {
-  printer("Hello, World!")
+ value1,value2 := printer("Hello, World!")
+
+  if (value2 == nil ) {
+    fmt.Printf("I am in if \n")
+    fmt.Printf(" %s \n", value1 )
+  } else {
+    fmt.Printf("I am in else \n")
+    fmt.Printf(" %s %s", value1, value2 )
+  }
 }
