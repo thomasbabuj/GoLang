54327468d47bbb07b69a2f41a758e5c6fcad46fc
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 11:24:12 +0800

    fix the error by adding the else

--- src/hello/main.go
+++ src/hello/main.go
@@ -9,9 +9,11 @@ func main() {
   // Go Printf returns also returns errors
   if  numberBytes, theError := fmt.Printf("Hello, world! \n"); theError != nil {
     os.Exit(1)
+  } else {
+     // since numberBytes was declared in if scope so its not available here
+    fmt.Printf("Printed %d bytes \n", numberBytes)
   }
 
-  // since numberBytes was declared in if scope so its not available here
-  fmt.Printf("Printed %d bytes \n", numberBytes)
+
 
 }
