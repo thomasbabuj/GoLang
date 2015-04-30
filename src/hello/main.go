955535f131d794c7d67b160de36c39ffc10b586f
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 15:28:27 +0800

    simple array

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,16 +4,12 @@ import (
   "fmt"
 )
 
-// Using Defer keyword
-// Passing unlimited nos of parameters to a function
-
-func printer(format string, msgs ...string) {
-  for _, msg := range msgs {
-    fmt.Printf(format, msg)
-  }
-}
+// Array
 
 func main() {
-  printer("%x \n", "Hello world!", "How are you?")
+    words := [...]string{"the", "quick", "brown", "fox"}
+    fmt.Printf("%s \n", words[2])
 }
+
+
 
