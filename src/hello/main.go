12bb78355fd8cd334c767c68087a1930c0adf1b0
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 15:31:23 +0800

    no specific array size

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,9 +4,12 @@ import (
   "fmt"
 )
 
-// Array
+// Array , Slices
+// Built-in types
 
 func main() {
+    // since we are using "..." so we can store more values
+    // there is no specific array size in this case
     words := [...]string{"the", "quick", "brown", "fox"}
     fmt.Printf("%s \n", words[2])
 }
