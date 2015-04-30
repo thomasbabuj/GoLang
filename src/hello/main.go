cf86efb31591e0629278f972b3bcf4700287cb68
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 11:54:50 +0800

    like normal c like for

--- src/hello/main.go
+++ src/hello/main.go
@@ -8,12 +8,7 @@ func main() {
 
   // Looping in Go
   // for is like a while loop
-  var counter int
-
-  counter = 0
-
-  for counter <  10 {
-    counter += 1
+  for counter:=0; counter <  10; counter +=1 {
     fmt.Printf("Hello, World \n ")
   }
 
