73484b007181acc52edc64b10c6739f0fd07cdb1
Thomas Babu <tommy.developer@gmail.com>
2015-04-30 12:45:20 +0800

    multiple defer keywords

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,11 +4,13 @@ import (
   "fmt"
 )
 
-// Using Deffer keyword
+// Using Defer keyword
 //  defer section will be called after the return statement in a function
  func printer(msg string) error {
    // here we are adding new line after the function returns value
+   // we can also add multiple defer. but it will stack up and follow a certain order when it runs
    defer fmt.Printf("\n")
+   defer fmt.Printf("Add more \n")
   _, err := fmt.Printf("%s", msg)
 
   return err
