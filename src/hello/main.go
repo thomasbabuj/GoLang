39caa1fba5c05bd59dccda1cf3fe6199649427f1
Thomas Babu <tommy.developer@gmail.com>
2015-05-01 23:54:53 +0800

    using errors package

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+)
+
+// Import errors pacakge and declare a string variable with the error msg
+var (
+	errorEmptyString = errors.New("Unwilling to print an empty string")
 )
 
 // Errors
@@ -12,13 +18,17 @@ import (
 * a bad idea to use exeception.
 *
 * For custom errors, we can use fmt package "Errorf" function
+* Another way to defined errors is to use Error package. the advantage here is
+* we can assigned what type of error etc..
+*
  */
 
 // Built in type error handling
 func printer(msg string) error {
 	// Using Fmt.Errorf to give custom error msg
 	if msg == "" {
-		return fmt.Errorf("Unwilling to print an empty string")
+		// use the error variable declared
+		return errorEmptyString
 	}
 	_, err := fmt.Printf("%s \n", msg)
 
