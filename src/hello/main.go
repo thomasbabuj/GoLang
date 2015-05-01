3f5f0cb427008c2d9693bb6eccbd8c26bfa4b363
Thomas Babu <tommy.developer@gmail.com>
2015-05-01 23:48:39 +0800

    used fmt.Errorf function to assign custom error

--- src/hello/main.go
+++ src/hello/main.go
@@ -10,17 +10,24 @@ import (
 /*
 * Go programs are not built around with exceptions. its
 * a bad idea to use exeception.
+*
+* For custom errors, we can use fmt package "Errorf" function
  */
 
 // Built in type error handling
 func printer(msg string) error {
+	// Using Fmt.Errorf to give custom error msg
+	if msg == "" {
+		return fmt.Errorf("Unwilling to print an empty string")
+	}
 	_, err := fmt.Printf("%s \n", msg)
 
 	return err
 }
 
 func main() {
-	if err := printer("Hello, world!"); err != nil {
+	if err := printer(""); err != nil {
+		fmt.Printf("printer failed: %s\n", err)
 		os.Exit(1)
 	}
 }
