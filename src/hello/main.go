fca364bc282cc647edd2883d3932d98d85415044
Thomas Babu <tommy.developer@gmail.com>
2015-05-04 16:11:38 +0800

    Creating a channel with integers

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,30 +2,22 @@ package main
 
 import (
     "fmt"
+    "math/rand"
 )
 
-func emit(c chan string) {
-    words := []string{"the", "quick", "brown", "fox"}
-
-    for _, word := range words {
-        // sending to channel
-        c  <- word
+func makeRandoms(c chan int) {
+    for {
+        c <- rand.Intn(1000)
     }
-
 }
 
 func main() {
-    // Creating a channel
-    wordChannel := make(chan string)
 
-    // starting multiple channels
-    go emit(wordChannel)
-    go emit(wordChannel)
+    randoms := make(chan int)
 
-    // this code got deadlock error mainly because, this range wordchannel is waiting for
-    // response from emit channel, even though the channel finished running.
-    for word := range wordChannel {
-        fmt.Printf("%s ", word)
+    go makeRandoms(randoms)
+
+    for n := range randoms {
+        fmt.Printf("%d ", n)
     }
-
 }
