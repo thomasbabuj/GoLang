e69a782a3b1fac0047d6600fd6017854e7a51513
Thomas Babu <tommy.developer@gmail.com>
2015-05-04 15:30:53 +0800

    Basic channel with range keyword

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,52 +1,31 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os"
+    "fmt"
 )
 
-// Import errors pacakge and declare a string variable with the error msg
-var (
-	errorEmptyString = errors.New("Unwilling to print an empty string")
-)
+func emit(c chan string) {
+    words := []string{"the", "quick", "brown", "fox"}
 
-// Errors
+    for _, word := range words {
+        // sending to channel
+        c  <- word
+    }
 
-/*
-* Go programs are not built around with exceptions. its
-* a bad idea to use exeception.
-*
-* For custom errors, we can use fmt package "Errorf" function
-* Another way to defined errors is to use Error package. the advantage here is
-* we can assigned what type of error etc..
-*
-* Go also has exception handling mechanics, which is called panic and recover
-* which we use very rarely. Only use panic when we need to terminate the app.
-*
-*
- */
-
-// Built in type error handling
-func printer(msg string) error {
-	// Using Fmt.Errorf to give custom error msg
-	if msg == "" {
-		// use the error variable declared
-		panic(errorEmptyString)
-	}
-	_, err := fmt.Printf("%s \n", msg)
-
-	return err
+    close(c)
 }
 
 func main() {
-	if err := printer(""); err != nil {
-		// since we used error pcakge, now we can compare the error msg
-		if err == errorEmptyString {
-			fmt.Printf("You rited to print a empty string!")
-		} else {
-			fmt.Printf("printer failed: %s\n", err)
-		}
-		os.Exit(1)
-	}
+    // Creating a channel
+    wordChannel := make(chan string)
+
+    go emit(wordChannel)
+
+    // receive the things comes from the wordChannel
+    // Here we are using range to receive from channel
+    for word := range wordChannel {
+        fmt.Printf("%s ", word)
+    }
+
+    fmt.Printf("\n")
 }
