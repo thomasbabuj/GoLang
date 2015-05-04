c8c45548b596b8881e5f4b23ac156a9fe61c477b
Thomas Babu <tommy.developer@gmail.com>
2015-05-04 18:07:38 +0800

    Basic code to get page info

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,57 +1,41 @@
+/*
+   Multiple Readers, Multiple Writers
+   In this program we will fetch list of webpages and returns the size
+ */
+
 package main
 
 import (
     "fmt"
-    "time"
+    "io/ioutil"
+    "net/http"
+    "os"
 )
 
-// Channel of channels
-// Here the first param is channel of channels, so to inside this function we are creating a new channel for word and
-// send the wordchannel back to the main function.
+func getPage(url string) (int, error) {
+    // Using GET method from http
+    resp, err := http.Get(url)
 
-func emit(chanChannel chan chan string, done chan bool) {
-
-    wordChannel := make(chan string)
-    chanChannel <- wordChannel
-
-    defer close(wordChannel)
-
-    words := []string{"the", "quick", "brown", "fox"}
-    i := 0
-
-    t := time.NewTimer(3 * time.Second)
-
-    // infinite loop, if somebody is listening to receive  then they will get the next word
-    // and if it receives done channel then it terminates the channel.
-    for {
-        select {
-            case wordChannel <- words[i] :
-                i += 1
-                if i == len(words) {
-                    i = 0
-                }
-
-            case <- done:
-                fmt.Printf(" Go done \n")
-                done<-true
-                return
-            case <- t.C :
-                fmt.Printf(" \n Timer terminates the channel \n")
-                return
-        }
+    if err != nil {
+        return 0, err
     }
+
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return 0, err
+    }
+
+    return len(body), nil
 }
 
 func main() {
-    channelCh:= make(chan chan string)
-    doneCh := make(chan bool)
+    url := "http://www.google.com/"
 
-    go emit(channelCh, doneCh)
-
-    wordCh := <-channelCh
-
-    for word := range wordCh {
-        fmt.Printf("%s ", word)
+    pageLength, err := getPage(url)
+    if err != nil {
+        os.Exit(1)
     }
-}
 
+    fmt.Printf("%s is length %d \n", url, pageLength )
+}
